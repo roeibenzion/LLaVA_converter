11954047cb3907d34e4c362deab3adaaf09f29ea
Roei Ben Zion <roei.benzion@gmail.com>
2024-11-12 11:19:49 +0200

    Increase max len to 1024 to avoid loss = 0.0

--- scripts/v1_5/reconst_pretrain.sh
+++ scripts/v1_5/reconst_pretrain.sh
@@ -28,7 +28,7 @@ deepspeed llava/train/train_mem.py \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
     --tf32 True \
-    --model_max_length 512 \
+    --model_max_length 1024 \
     --gradient_checkpointing True \
     --dataloader_num_workers 4 \
     --lazy_preprocess True \
