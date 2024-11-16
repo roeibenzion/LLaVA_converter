d9ded4bf336ea1105b8445fbf742704cc3762b97
Roei Ben Zion <roei.benzion@gmail.com>
2024-11-16 12:39:44 +0200

    Save steps

--- scripts/v1_5/pretrain_tune_data.sh
+++ scripts/v1_5/pretrain_tune_data.sh
@@ -20,7 +20,7 @@ deepspeed llava/train/train_mem.py \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 24000 \
+    --save_steps 500 \
     --save_total_limit 1 \
     --learning_rate 1e-3 \
     --weight_decay 0. \
