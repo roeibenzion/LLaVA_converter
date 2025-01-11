aeb4d6cecc8b942e7ed72d5878ae4120f3703eb9
Roei Ben Zion <roei.benzion@gmail.com>
2025-01-11 17:48:22 +0200

    regular file

--- scripts/v1_5/eval/textvqa.sh
+++ scripts/v1_5/eval/textvqa.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 python -m llava.eval.model_vqa_loader \
-    --model-path /content/LLaVA_converter/checkpoints/checkpoint-500 \
+--model-path ./checkpoints/llava-v1.5-7b-lora \
+--model-base lmsys/vicuna-7b-v1.5 \
     --model-base lmsys/vicuna-7b-v1.5 \
-    --question-file ./playground/data/eval/textvqa/test.json \
+    --question-file ./playground/data/eval/textvqa/llava_textvqa_val_v051_ocr.jsonl \
     --image-folder ./playground/data/eval/textvqa/train_images \
     --answers-file ./playground/data/eval/textvqa/answers/llava-v1.5-7b.jsonl \
     --temperature 0 \
     --conv-mode vicuna_v1 \
-    --q_limit 300
 
 python -m llava.eval.eval_textvqa \
     --annotation-file ./playground/data/eval/textvqa/TextVQA_0.5.1_val.json \
