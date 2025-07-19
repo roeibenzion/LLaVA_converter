e49d89a63aa59b4af39183b9e6ecdf3502d7694b
Roei Ben Zion <roei.benzion@gmail.com>
2025-07-19 18:25:04 +0300

    fix

--- llava/model/multimodal_projector/fga/attn.py
+++ llava/model/multimodal_projector/fga/attn.py
@@ -118,6 +118,8 @@ class Atten(nn.Module):
         self.similar_modalities = similar_modalities
         # One representative for each group of similar modalities, the value is the modalities connected to it.
         self.similar_modalities_reps = {rep[0]:[] for rep in self.similar_modalities}
+        for i, rep in enumerate(self.similar_modalities_reps.keys()):
+            self.similar_modalities_reps[rep] = [idx for idx in self.similar_modalities[i] if idx != rep]
         for rep in self.similar_modalities_reps.keys():
             for idx in self.similar_modalities[rep]:
                 self.similar_modalities_reps[rep].append(idx)
