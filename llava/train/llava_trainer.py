5067e59b205a1eff4391869ba8138e714346256a
Roei Ben Zion <roei.benzion@gmail.com>
2025-07-25 09:35:01 +0300

    name for save

--- llava/train/llava_trainer.py
+++ llava/train/llava_trainer.py
@@ -439,7 +439,7 @@ class LLaVATrainer(Trainer):
                 # self.model.config.save_pretrained(output_dir)
                 config = _sanitize_config(self.model.config)
                 config.save_pretrained(output_dir)
-                torch.save(weight_to_save, os.path.join(output_dir, f'mm_projector.bin'))
+                torch.save(weight_to_save, os.path.join(output_dir, f'llava_model.bin'))
         else:
             super(LLaVATrainer, self)._save_checkpoint(model, trial, metrics)
 
