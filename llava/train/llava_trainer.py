3f9f5e5571b129a94c4937bc25bfc6890b595791
Roei Ben Zion <roei.benzion@gmail.com>
2025-07-26 09:32:43 +0300

    debug

--- llava/train/llava_trainer.py
+++ llava/train/llava_trainer.py
@@ -431,7 +431,7 @@ class LLaVATrainer(Trainer):
             output_dir = os.path.join(run_dir, checkpoint_folder)
 
             # Save adapter weights only
-            keys_to_match = ['mm_projector', 'vision_resampler']
+            keys_to_match = ['mm_projector', 'vision_resampler', 'atten']
             if getattr(self.args, "use_im_start_end", False):
                 keys_to_match.extend(['embed_tokens', 'embed_in'])
             
