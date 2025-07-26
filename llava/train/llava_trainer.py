48fc4d0f4e5bb8305f4e2426d25b8123afda9de9
Roei Ben Zion <roei.benzion@gmail.com>
2025-07-26 09:35:45 +0300

    fix for save

--- llava/train/llava_trainer.py
+++ llava/train/llava_trainer.py
@@ -435,8 +435,6 @@ class LLaVATrainer(Trainer):
             if getattr(self.args, "use_im_start_end", False):
                 keys_to_match.extend(['embed_tokens', 'embed_in'])
             
-            if getattr(self.args, "fga", False):
-                keys_to_match.extend(['atten'])
 
             weight_to_save = get_mm_adapter_state_maybe_zero_3(model.named_parameters(), keys_to_match)
 
