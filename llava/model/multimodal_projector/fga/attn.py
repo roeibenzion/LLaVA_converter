a99f1ada6b67780e2f53a5469a80e07228acf5cf
Roei Ben Zion <roei.benzion@gmail.com>
2025-08-01 16:02:49 +0300

    try

--- llava/model/multimodal_projector/fga/attn.py
+++ llava/model/multimodal_projector/fga/attn.py
@@ -49,7 +49,7 @@ class Pairwise(nn.Module):
         self.embed_Y = nn.Conv1d(embed_y_size, self.embed_size, 1)
         if x_spatial_dim is not None:
             print(f"This is batch norm with {x_spatial_dim} x {self.y_spatial_dim} spatial dimensions")
-            self.normalize_S = nn.BatchNorm1d(self.x_spatial_dim * self.y_spatial_dim)
+            self.normalize_S = nn.BatchNorm1d(self.x_spatial_dim * self.y_spatial_dim, track_running_stats=False)
 
             self.margin_X = nn.Conv1d(self.y_spatial_dim, 1, 1)
             self.margin_Y = nn.Conv1d(self.x_spatial_dim, 1, 1)
